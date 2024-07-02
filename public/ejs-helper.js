2a7ef374296e2fb3db899f289135883036c9559d
Audrico <67388541+DricZ@users.noreply.github.com>
2024-07-02 19:08:12 +0700

    gf

--- public/ejs-helper.js
+++ public/ejs-helper.js
@@ -8,4 +8,8 @@ module.exports = {
     const item = items.find(val => val.item_id === itemId && val[field]);
     return item ? item[field] : '';
   },
+  findValue2: (items, itemId, field) => {
+    const item = items.find(val => val.vendor_id === itemId && val[field]);
+    return item ? item[field] : '';
+  },
 };
